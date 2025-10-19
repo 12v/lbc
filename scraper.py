6d6f6646cec733e3db985025fade068bcbbab190
12v <11772383+12v@users.noreply.github.com>
2025-10-19 15:05:40 +0100

    Update cron schedule to run every 10 minutes and increase max runtime to 10 minutes

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
 }
 STATE_FILE = Path("state.txt")
-MAX_RUNTIME_SECS = 2 * 60
+MAX_RUNTIME_SECS = 10 * 60
 
 
 def get_cache_path(slug):
